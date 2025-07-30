aeaf96dfd5bfbba328ce7bf98965490f16a4b09c
nik26788 <risinnshow@gmail.com>
2025-07-30 15:58:02 +0800

    update

--- apps/builder/src/App.vue
+++ apps/builder/src/App.vue
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
         src="/vite.svg"
         class="logo"
         alt="Vite logo"
-      >
+      />
     </a>
     <a
       href="https://vuejs.org/"
@@ -22,7 +22,7 @@ import HelloWorld from './components/HelloWorld.vue'
         src="./assets/vue.svg"
         class="logo vue"
         alt="Vue logo"
-      >
+      />
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
--- eslint.config.js
+++ eslint.config.js
@@ -5,37 +5,36 @@ import js from '@eslint/js'
 import vueEslintParser from 'vue-eslint-parser'
 import tsParser from '@typescript-eslint/parser'
 export default [
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-                defineProps: 'readonly',
-                defineEmits: 'readonly'
-            }
-        }
-    },
-    {
-        files: ['**/*.{ts,tsx,vue}'],
-        rules: {
-            ...js.configs.recommended.rules,
-            ...pluginVue.configs['vue3-strongly-recommended'].rules,
-            indent: ['error', 2],
-            'prefer-const': 'error',
-            'no-console': 'error',
-            'vue/valid-define-emits': 'error',
-            'simple-import-sort/imports': 'error',
-            'simple-import-sort/exports': 'error'
-        },
-        languageOptions: {
-            parser: vueEslintParser,
-            parserOptions: {
-                extraFileExtensions: ['.vue'],
-                parser: tsParser
-            }
-        },
-        plugins: {
-            vue: pluginVue,
-            'simple-import-sort': simpleImportSort
-        }
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        defineProps: 'readonly',
+        defineEmits: 'readonly'
+      }
     }
+  },
+  {
+    files: ['**/*.{ts,tsx,vue}'],
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginVue.configs['vue3-strongly-recommended'].rules,
+      'prefer-const': 'error',
+      'no-console': 'error',
+      'vue/valid-define-emits': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    },
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        extraFileExtensions: ['.vue'],
+        parser: tsParser
+      }
+    },
+    plugins: {
+      vue: pluginVue,
+      'simple-import-sort': simpleImportSort
+    }
+  }
 ]
